5782389f0b54148c55f8a441996c415faf790446
KARAN VAGHELA <karanmvaghela@gmail.com>
2024-12-20 17:06:38 +0530

    Done

---
+++ src/Function_Color.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+
+public class Function_Color {
+    GUI gui;
+
+    public Function_Color(GUI gui) {
+        this.gui = gui;
+    }
+
+    public void changeColor(String color){
+        switch (color){
+            case "White":
+                gui.window.getContentPane().setBackground(Color.WHITE);
+                gui.textArea.setBackground(Color.WHITE);
+                gui.textArea.setForeground(Color.BLACK);
+                break;
+
+            case "Black":
+                gui.window.getContentPane().setBackground(Color.BLACK);
+                gui.textArea.setBackground(Color.BLACK);
+                gui.textArea.setForeground(Color.WHITE);
+                break;
+
+            case "Blue":
+                gui.window.getContentPane().setBackground(Color.BLUE);
+                gui.textArea.setBackground(Color.BLUE);
+                gui.textArea.setForeground(Color.WHITE);
+                break;
+
+            case "Hacker":
+                gui.window.getContentPane().setBackground(Color.BLACK);
+                gui.textArea.setBackground(Color.BLACK);
+                gui.textArea.setForeground(Color.GREEN);
+                break;
+
+        }
+    }
+}
--- src/GUI.java
+++ src/GUI.java
@@ -5,19 +5,38 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.undo.UndoManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
 public class GUI implements ActionListener {
+    //Text Area
     JFrame window;
     JTextArea textArea;
     JScrollPane scrollPane;
+    boolean wordWrapOn = false;
+    //Top Menu bar
     JMenuBar menuBar;
     JMenu menuFile, menuEdit, menuFormat, menuColor;
     JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
+    //Edit Menu
+    JMenuItem iUndo,iRedo;
+    //Format Menu
+    JMenuItem iWrap, iFontArial, iFontCSMS, iFontTNR, iFontSize8, iFontSize12, iFontSize16, iFontSize20, iFontSize24, iFontSize28;
+    JMenu menuFont, menuFontSize;
+    //Color Menu
+    JMenuItem icolor1, icolor2, icolor3, icolor4;
 
-    Funcition_File file = new Funcition_File(this);
+    Function_File file = new Function_File(this);
+    Function_Format format = new Function_Format(this);
+    Function_Color color = new Function_Color(this);
+    Function_Edit edit   = new Function_Edit(this);
+
+
+    UndoManager um = new UndoManager();
 
     public static void main(String[] args) {
         new GUI();
@@ -28,6 +47,13 @@ public class GUI implements ActionListener {
         createTextArea();
         createMenuBar();
         createFileMenu();
+        createEditMenu();
+        createFormatMenu();
+        createColorMenu();
+        format.selectFont = "Arial";
+        format.createFont(16);
+        format.wordWrap();
+        color.changeColor("White");
         window.setVisible(true);
     }
 
@@ -39,6 +65,14 @@ public class GUI implements ActionListener {
 
     public void createTextArea() {
         textArea = new JTextArea();
+
+        textArea.getDocument().addUndoableEditListener(new UndoableEditListener() {
+            @Override
+            public void undoableEditHappened(UndoableEditEvent e) {
+                um.addEdit(e.getEdit());
+            }
+        });
+
         scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         window.add(scrollPane);
@@ -67,12 +101,114 @@ public class GUI implements ActionListener {
         iOpen.addActionListener(this);
         iOpen.setActionCommand("Open");
         menuFile.add(iOpen);
-//        iSave = new JMenuItem("Save");
-//        menuFile.add(iSave);
-//        iSaveAs = new JMenuItem("SaveAs");
-//        menuFile.add(iSaveAs);
-//        iExit = new JMenuItem("Exit");
-//        menuFile.add(iExit);
+        iSave = new JMenuItem("Save");
+        iSave.addActionListener(this);
+        iSave.setActionCommand("Save");
+        menuFile.add(iSave);
+        iSaveAs = new JMenuItem("SaveAs");
+        iSaveAs.addActionListener(this);
+        iSaveAs.setActionCommand("SaveAs");
+        menuFile.add(iSaveAs);
+        iExit = new JMenuItem("Exit");
+        iExit.addActionListener(this);
+        iExit.setActionCommand("Exit");
+        menuFile.add(iExit);
+
+    }
+
+    public void createEditMenu(){
+        iUndo = new JMenuItem("Undo");
+        iUndo.addActionListener(this);
+        iUndo.setActionCommand("Undo");
+        menuEdit.add(iUndo);
+
+        iRedo = new JMenuItem("Redo");
+        iRedo.addActionListener(this);
+        iRedo.setActionCommand("Redo");
+        menuEdit.add(iRedo);
+
+    }
+
+    public void createFormatMenu(){
+        iWrap = new JMenuItem("Word Wrap: OFF");
+        iWrap.addActionListener(this);
+        iWrap.setActionCommand("Word Wrap");
+        menuFormat.add(iWrap);
+
+        menuFont = new JMenu("Font");
+        menuFormat.add(menuFont);
+
+        iFontArial = new JMenuItem("Arial");
+        iFontArial.addActionListener(this);
+        iFontArial.setActionCommand("Arial");
+        menuFont.add(iFontArial);
+
+        iFontCSMS = new JMenuItem("Comic Sans MS");
+        iFontCSMS.addActionListener(this);
+        iFontCSMS.setActionCommand("Comic Sans MS");
+        menuFont.add(iFontCSMS);
+
+        iFontTNR = new JMenuItem("Times New Roman");
+        iFontTNR.addActionListener(this);
+        iFontTNR.setActionCommand("Times New Roman");
+        menuFont.add(iFontTNR);
+
+        menuFontSize = new JMenu("Font Size");
+        menuFormat.add(menuFontSize);
+
+        iFontSize8 = new JMenuItem("8");
+        iFontSize8.addActionListener(this);
+        iFontSize8.setActionCommand("8");
+        menuFontSize.add(iFontSize8);
+
+        iFontSize12 = new JMenuItem("12");
+        iFontSize12.addActionListener(this);
+        iFontSize12.setActionCommand("12");
+        menuFontSize.add(iFontSize12);
+
+        iFontSize16 = new JMenuItem("16");
+        iFontSize16.addActionListener(this);
+        iFontSize16.setActionCommand("16");
+        menuFontSize.add(iFontSize16);
+
+        iFontSize20 = new JMenuItem("20");
+        iFontSize20.addActionListener(this);
+        iFontSize20.setActionCommand("20");
+        menuFontSize.add(iFontSize20);
+
+        iFontSize24 = new JMenuItem("24");
+        iFontSize24.addActionListener(this);
+        iFontSize24.setActionCommand("24");
+        menuFontSize.add(iFontSize24);
+
+        iFontSize28 = new JMenuItem("28");
+        iFontSize28.addActionListener(this);
+        iFontSize28.setActionCommand("28");
+        menuFontSize.add(iFontSize28);
+
+    }
+
+    public void createColorMenu(){
+        icolor1 = new JMenuItem("White");
+        icolor1.addActionListener(this);
+        icolor1.setActionCommand("White");
+        menuColor.add(icolor1);
+
+        icolor2 = new JMenuItem("Black");
+        icolor2.addActionListener(this);
+        icolor2.setActionCommand("Black");
+        menuColor.add(icolor2);
+
+        icolor3 = new JMenuItem("Blue");
+        icolor3.addActionListener(this);
+        icolor3.setActionCommand("Blue");
+        menuColor.add(icolor3);
+
+        icolor4 = new JMenuItem("Hacker");
+        icolor4.addActionListener(this);
+        icolor4.setActionCommand("Hacker");
+        menuColor.add(icolor4);
+
 
     }
 
@@ -80,10 +216,27 @@ public class GUI implements ActionListener {
     public void actionPerformed(@org.jetbrains.annotations.NotNull ActionEvent e) {
         String command = e.getActionCommand();
         switch (command) {
-            case "New":
-                file.newFle();
-                break;
-            case "Open": file.open();break;
+            case "New": file.newFle();break;
+            case "Open":file.open();break;
+            case "Save":file.save();break;
+            case "SaveAs":file.saveAs();break;
+            case "Exit":file.exit();break;
+            case "Undo":edit.undo();break;
+            case "Redo":edit.redo();break;
+            case "Word Wrap":format.wordWrap();break;
+            case "Arial":format.setFont(command);break;
+            case "Comic Sans MS":format.setFont(command);break;
+            case "Times New Roman":format.setFont(command);break; 
+            case "8":format.createFont(8);break;
+            case "12":format.createFont(12);break;
+            case "16":format.createFont(16);break;
+            case "20":format.createFont(20);break;
+            case "24":format.createFont(24);break;
+            case "28":format.createFont(28);break;
+            case "White":color.changeColor("White");break;
+            case "Black":color.changeColor("Black");break;
+            case"Blue":color.changeColor("Blue");break;
+            case "Hacker":color.changeColor("Hacker");break;
         }
     }
 }
